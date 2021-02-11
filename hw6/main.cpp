7859711e2120f82cdc2d912760eb378f78a1b045
alvinjj <alvinjasperj@yahoo.com>
2021-02-11 05:43:14 -0800

    Initial Commit

---
+++ hw6/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hw6
+//
+//  Created by Alvin J on 2/11/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
